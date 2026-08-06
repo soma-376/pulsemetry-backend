1f1ad7cd455b8554da293a1729d659ecd3ef621a
Jaewon Kim <eshcirc1e@gmail.com>
2026-08-07 01:20:50 +0900

    init: 프로젝트 생성

---
+++ src/test/kotlin/com/team376/pulsemetry/PulsemetryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.team376.pulsemetry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PulsemetryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
